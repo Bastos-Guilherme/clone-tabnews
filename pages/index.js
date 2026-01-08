5a47e9bfdbbcd3e20a36e7e6567845be0617af5e
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2026-01-08 22:25:01 +0000

    Altera frase do `index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Bom tarde!</h1>;
+  return (
+    <h1>
+      Bom tarde! Este agora eh um endereco web valido 24/7, espero que entenda o
+      significado disso
+    </h1>
+  );
 }
 
 export default Home;
