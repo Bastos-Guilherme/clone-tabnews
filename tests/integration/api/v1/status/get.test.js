f139ccd3b9dc256d8695020a4a9ba8a66d8a93a0
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2026-01-12 20:47:52 +0000

    Adiciona `.env

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,6 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("https://localhost:3000/api/v1/status");
+  const response = await fetch(
+    "https://obscure-space-journey-q5p9vj7x4p72x7g7-3000.app.github.dev/api/v1/status",
+  );
   expect(response.status).toBe(200);
 });
