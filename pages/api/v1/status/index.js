af0af7be78fc985b3bca13600d39bf4f4cc54121
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2026-01-14 20:31:37 +0000

    Adicionado try/catch na connexao do banco

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ status: "UP" });
+  const updatedAt = new Date().toISOString();
+
+  const rawDbVersion = await database.query("SHOW server_version;");
+  const dbVersion = rawDbVersion.rows[0].server_version;
+
+  const rawDbMaxConnections = await database.query("SHOW max_connections;");
+  const dbMaxConnections = rawDbMaxConnections.rows[0].max_connections;
+
+  const dbName = "local_db";
+  const rawDbOpenedConnections = await database.query(
+    `SELECT count(*)::int FROM pg_stat_activity WHERE datname='${dbName}';`,
+  );
+  const dbOpenedConnections = rawDbOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
